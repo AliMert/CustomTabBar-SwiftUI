c742d3386954d576a5cf92978c894d32a5dd86c2
Ali Mert Özhayta <mozhayta@gmail.com>
2023-06-12 22:20:14 +0300

    Initial Commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by Ali Mert Özhayta on 12.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
